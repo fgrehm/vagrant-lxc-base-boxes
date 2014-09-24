4546b8e9db792f3840202199aa06f15e75a68c7c
Fabio Rehm <fgrehm@gmail.com>
2014-09-24 12:40:53 -0300

    Update centos packages before installing new ones

--- centos/install-extras.sh
+++ centos/install-extras.sh
@@ -13,6 +13,8 @@ utils.lxc.start
 log 'Sleeping for 10 seconds...'
 sleep 10
 
+utils.lxc.attach yum update -y
+
 # TODO: Support for appending to this list from outside
 PACKAGES=(vim curl wget man-db bash-completion python-software-properties ca-certificates sudo nfs-common)
 utils.lxc.attach yum install install ${PACKAGES[*]} -y

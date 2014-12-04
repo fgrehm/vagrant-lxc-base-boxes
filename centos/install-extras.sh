6fddcfec720bc9be80fe5620eabff88c27ae4637
Jan Collijs <jan.collijs@inuits.eu>
2014-12-04 14:09:10 +0100

    Feature: puppet provisioner for centos boxes

--- centos/install-extras.sh
+++ centos/install-extras.sh
@@ -18,3 +18,18 @@ utils.lxc.attach yum update -y
 # TODO: Support for appending to this list from outside
 PACKAGES=(vim curl wget man-db bash-completion python-software-properties ca-certificates sudo nfs-common openssh-server)
 utils.lxc.attach yum install ${PACKAGES[*]} -y
+
+# Puppet provisioner
+PUPPET=${PUPPET:=0}
+
+if [ $PUPPET = 1 ]; then
+  if $(lxc-attach -n ${CONTAINER} -- which puppet &>/dev/null); then
+    log "Puppet has been installed on container, skipping"
+  else
+    log "Installing Puppet"
+    wget http://yum.puppetlabs.com/puppetlabs-release-el-${RELEASE}.noarch.rpm -O "${ROOTFS}/tmp/puppetlabs-release-stable.rpm" &>>${LOG}
+    utils.lxc.attach rpm -Uvh "/tmp/puppetlabs-release-stable.rpm"
+    utils.lxc.attach yum update -y
+    utils.lxc.attach yum install puppet -y
+  fi
+fi
